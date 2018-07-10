bf6406e099a7f81740c4a6896665b9613b2d787b
Joanna Coll <johannam999@gmail.com>
2018-07-10 11:25:33 -0700

    scaffolding

---
+++ src/routes.js
@@ -0,0 +1,6 @@
+export const LOGIN_ROUTE = '/login';
+export const SIGNUP_ROUTE = '/signup';
+export const ROOT_ROUTE = '/';
+export const DASHBOARD_ROUTE = '/dashboard';
+export const PROFILE_ROUTE = '/profile';
+export const PLANT_ROUTE = '/plants';
