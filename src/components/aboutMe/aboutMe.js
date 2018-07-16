ae1ff080ec0515da5e532cf001d869b7e642c92b
Joanna Coll <johannam999@gmail.com>
2018-07-15 22:00:32 -0700

    about me started

---
+++ src/components/aboutMe/aboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import joannaPicture from '../../assets/joanna.png';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2></h2>
+        <img src={joannaPicture} alt='joanna picture'/>
+      </div>
+    );
+  }
+}
+
+export default About;
