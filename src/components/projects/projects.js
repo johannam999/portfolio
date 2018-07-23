5c5c0d795700a63b2eecddef4e5b5c0debf2c64b
Joanna Coll <johannam999@gmail.com>
2018-07-22 22:48:47 -0700

    typos

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -11,7 +11,8 @@ class Projects extends React.Component {
       <div>
           <h2>My Projects</h2>
         <div>
-          <h1><a href='http://www.musicpic.fun'> Music Pic</a><img src={logoMP}width='90px'/></h1>         <p>An app that uses Azure face recognition to display Spotify playlist based on keyword.</p>
+          <h1><a href='http://www.musicpic.fun'> Music Pic</a><img src={logoMP}width='90px'/></h1> 
+          <p>An app that uses Azure face recognition to display Spotify playlist based on keyword.</p>
         </div>
         <div>
           <h1><a href='https://github.com/Bloomio '> Bloomio </a><img src={bloomioLogo} width='100px'/></h1>
@@ -19,7 +20,8 @@ class Projects extends React.Component {
           </p>
         </div>
         <div>
-          <h1><a href='http://playandrest.us/'> Play and Rest </a><img src={logoPR}width='100px'/></h1>         <p>The app provides information to users about local play areas, dog off leash areas or waterfront. User can use google map search and filter by type and location.
+          <h1><a href='http://playandrest.us/'> Play and Rest </a><img src={logoPR}width='100px'/></h1>         
+          <p>The app provides information to users about local play areas, dog off leash areas or waterfront. User can use google map search and filter by type and location.
           </p>
         </div>
       </div>
