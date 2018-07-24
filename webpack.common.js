25874a2255677418b85f7eefd30ad77c2d40cdaf
Joanna Coll <johannam999@gmail.com>
2018-07-24 15:41:13 -0700

    webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,8 @@
 'use strict';
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const { DefinePlugin } = require('webpack');
+// const { DefinePlugin } = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssPlugin = require('mini-css-extract-plugin');
 
@@ -14,7 +14,6 @@ webpackConfig.entry = `${__dirname}/src/main.js`;
 webpackConfig.output = {
   filename: '[name].[hash].js',
   path: `${__dirname}/build`,
-  publicPath: process.env.CDN_URL,
 };
 
 webpackConfig.plugins = [
@@ -24,9 +23,9 @@ webpackConfig.plugins = [
   new MiniCssPlugin({
     filename: '[name].[hash].css',
   }),
-  new DefinePlugin({
-    API_URL: JSON.stringify(process.env.API_URL),
-  }),
+  // new DefinePlugin({
+  //   API_URL: JSON.stringify(process.env.API_URL),
+  // }),
 ];
 
 webpackConfig.module = {};
@@ -34,7 +33,7 @@ webpackConfig.module = {};
 
 webpackConfig.module.rules = [
   {
-    test: /\.(png|svg|jpg|gif)$/,
+    test: /\.(png|svg|jpg|gif|pdf)$/,
     use: [
       'file-loader',
     ],
