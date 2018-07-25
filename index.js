643a63400126f0e3b89af3ed2a078fd3cb4500ed
Joanna Coll <johannam999@gmail.com>
2018-07-25 13:47:11 -0700

    fixing

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(express.static(`${__dirname}/build`));
 
-app.get('/', (request, response) => {
+app.get('*', (request, response) => {
   response.sendFile(`${__dirname}/build/index.html`);
 });
 app.listen(process.env.PORT, () => {
-  console.log('__SERVER_UP___', process.env.PORT); // eslint-disable-line
+  console.log('__SERVER_UP___', `${process.env.PORT}`); // eslint-disable-line
 });
