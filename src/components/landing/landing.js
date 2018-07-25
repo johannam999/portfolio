057c2355436c9f3ad03aaaf56e2af2fb5e6aa1f3
Joanna Coll <johannam999@gmail.com>
2018-07-25 11:03:42 -0700

    changes in landing

---
+++ src/components/landing/landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div>
+
+      <h2>Hello, I am Joanna Coll - Software Dev. Welcome to my page! Please explore my projects.</h2>
+      </div>
+    );
+  }
+}
+
+export default Landing;
