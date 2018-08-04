522a9fbc6aa67d8291503a1709f57586ca562d37
Joanna Coll <johannam999@gmail.com>
2018-08-03 20:00:31 -0700

    fized deployment

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import * as routes from '../../routes';
 
 class Landing extends React.Component {
   render() {
     return (
       <div>
-
       <h2>Hello, I am Joanna Coll - Software Dev. Welcome to my page! Please explore my projects.</h2>
-      <button> Projects</button>
+      <button><Link to={routes.PROJECTS_ROUTE}> View my resume </Link>
+        </button>
       </div>
     );
   }
