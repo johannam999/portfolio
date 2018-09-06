79e1fb53f26752c1c85f7a1c38268a6f28d2061c
Joanna Coll <johannam999@gmail.com>
2018-09-05 21:11:34 -0700

    css

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as routes from '../../routes';
+import './landing.scss';
 
 class Landing extends React.Component {
   render() {
     return (
-      <div>
-      <h2>Hi, I am Joanna Coll - Software Developer. Welcome to my page! Please explore my projects.</h2>
-      <button><Link to={routes.PROJECTS_ROUTE}> Projects </Link>
-        </button>
+      <div className='landing'>
+      <h2> Hi,  
+        I am Joanna Coll, a Full-Stack Javascript Developer, based in Seattle, Washington. My passion is making sense of data, finding order in chaos, shipping products on time and designing clean, impactful user experiences.</h2>
+      <button><Link to={routes.PROJECTS_ROUTE}>    Learn more about me </Link>
+      </button>
       </div>
     );
   }
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -9,18 +9,18 @@ class Projects extends React.Component {
   render() {
     return (
       <div className='projects'>
-        <h2>My Projects</h2>
+        <h1>MY PROJECTS</h1>
         <div >
-          <h1><a href='http://www.musicpic.fun'> Music Pic</a><img src={logoMP}width='90px'/></h1> 
+          <h2><a href='http://www.musicpic.fun'> </a><img src={logoMP}width='90px'/></h2> 
           <p>An app that uses Azure face recognition to display Spotify playlist based on keyword.</p>
         </div>
         <div>
-          <h1><a href='https://github.com/Bloomio '> Bloomio </a><img src={bloomioLogo} width='100px'/></h1>
+          <h2><a href='https://github.com/Bloomio '></a><img src={bloomioLogo} width='100px'/></h2>
           <p>A backend app to manage and notify about plant watering schedule.
           </p>
         </div>
         <div>
-          <h1><a href='http://playandrest.us/'> Play and Rest </a><img src={logoPR}width='100px'/></h1>         
+          <h2><a href='http://playandrest.us/'> </a><img src={logoPR}width='100px'/></h2>         
           <p>The app provides information to users about local play areas, dog off leash areas or waterfront. User can use google map search and filter by type and location.
           </p>
         </div>
