86e541e490b5e09e2c6343413152816d60055f33
Joanna Coll <johannam999@gmail.com>
2018-09-08 23:15:53 -0700

    adding links svg

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,10 @@
 
 import React from 'react';
 
-import './footer.scss';
 import githubLogo from '../../assets/GitHub-Mark-32px.png';
 import linkedinLogo from '../../assets/linkedin-icon.png';
 
+import './footer.scss';
 
 class Footer extends React.Component {
   render() {
@@ -13,6 +13,7 @@ class Footer extends React.Component {
         <h2> © by Joanna Coll 2018</h2>
         
         <a        href='https://github.com/johannam999'><img src={githubLogo}/>
+        <GitHubLargeSVG className='other-svgs wiggle'/>
         </a>
         <a href='https://www.linkedin.com/in/joanna-coll/'><img src={linkedinLogo} width='49px'/>
         </a>
