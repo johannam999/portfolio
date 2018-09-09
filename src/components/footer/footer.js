f0251c0890592660d2470ec9f8c6428480ccb657
Joanna Coll <johannam999@gmail.com>
2018-09-08 22:57:47 -0700

    css

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -11,12 +11,12 @@ class Footer extends React.Component {
     return (
       <footer className='footer'>
         <h2> © by Joanna Coll 2018</h2>
-        <div>
+        
         <a        href='https://github.com/johannam999'><img src={githubLogo}/>
         </a>
         <a href='https://www.linkedin.com/in/joanna-coll/'><img src={linkedinLogo} width='49px'/>
         </a>
-        </div>
+        
       </footer>
       
     );
